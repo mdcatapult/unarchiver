5ff62103c5a386e103964033fed0bea2dd093b36
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2019-07-02 12:06:32 +0100

    updates for compression formats

--- build.sbt
+++ build.sbt
@@ -24,6 +24,8 @@ lazy val root = (project in file(".")).
       }
     },
     libraryDependencies ++= Seq(
+      "org.scalactic" %% "scalactic"                  % "3.0.5",
+      "org.scalatest" %% "scalatest"                  % "3.0.5" % "test",
       "com.typesafe.akka" %% "akka-slf4j"         % akkaVersion,
       "ch.qos.logback" % "logback-classic"        % "1.2.3",
       "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0",
@@ -31,11 +33,10 @@ lazy val root = (project in file(".")).
       "org.typelevel" %% "cats-macros"            % catsVersion,
       "org.typelevel" %% "cats-kernel"            % catsVersion,
       "org.typelevel" %% "cats-core"              % catsVersion,
-      "io.mdcatapult.klein" %% "queue"            % "0.0.2",
-      "io.mdcatapult.klein" %% "mongo"            % "0.0.1",
-      "org.apache.tika" % "tika-core"                 % tikaVersion,
-      "org.apache.tika" % "tika-parsers"              % tikaVersion,
-      "jakarta.ws.rs" % "jakarta.ws.rs-api" % "2.1.4"
+      "io.mdcatapult.doclib" %% "common"          % "0.0.5",
+      "org.apache.tika" % "tika-core"             % tikaVersion,
+      "org.apache.tika" % "tika-parsers"          % tikaVersion,
+      "jakarta.ws.rs" % "jakarta.ws.rs-api"       % "2.1.4"
     ).map(_ exclude("javax.ws.rs", "javax.ws.rs-api")),
     assemblyJarName := "consumer-unarchive.jar",
     assemblyMergeStrategy in assembly := {
---
+++ src/test/scala/io/mdcatapult/unarchive/extractors/AutoSpec.scala
@@ -0,0 +1,47 @@
+package io.mdcatapult.unarchive.extractors
+
+import java.io.File
+
+import org.apache.commons.io.FileUtils
+import org.scalatest.BeforeAndAfter
+
+class AutoSpec extends ExtractorSpec with BeforeAndAfter{
+
+  val files: List[(String, Int, String)] = List[(String, Int, String)](
+    // ("/test.bz2", 1, "application/x-bzip2"),
+    // ("/test.laz", 1, ""),
+    // ("/test.lzh", 3, "application/x-lzh-compressed"),
+    // ("/test.rar", 3, "application/x-rar"),
+    // ("/test-compressed.rar", 2, "application/x-rar-compressed"),
+    // ("/test.skyd", 3, "application/zlib"),
+    ("/test.tar", 1, "application/x-tar"),
+    // ("/test.tar.bz2", 1, "application/x-bzip2"),
+    // ("/test.wsz", 1, "application/zlib"),
+    ("/test.xz", 4, "application/x-xz"),
+    ("/test.zip", 1, "application/zip"),
+  )
+
+  files foreach  { file: (String, Int, String) ⇒ {
+    f"The file ${file._1}" should f"contain ${file._2} entries" in {
+      val result = new Auto(getPath(file._1)).getEntries
+      assert(result.nonEmpty)
+      assert(result.length == file._2)
+    }
+
+    it should "extract successfully " in {
+      val f = new Auto(getPath(file._1))
+      f.extract
+      val target = f.getTargetPath(getPath(file._1), config.getString("unarchive.to.path"))
+      assert(new File(target).exists())
+      assert(new File(target).listFiles().length > 0)
+    }
+  }}
+
+
+  after {
+    FileUtils.deleteDirectory(new File(config.getString("unarchive.to.path")))
+  }
+
+
+
+}
