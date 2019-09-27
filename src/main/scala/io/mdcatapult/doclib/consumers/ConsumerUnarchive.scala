50dfbe7a8185ea1bf750cf53cc53704a9a1c1f80
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2019-09-27 16:33:00 +0100

    Ensure queues are created

--- src/main/scala/io/mdcatapult/doclib/consumers/ConsumerUnarchive.scala
+++ src/main/scala/io/mdcatapult/doclib/consumers/ConsumerUnarchive.scala
@@ -8,6 +8,8 @@ import io.mdcatapult.doclib.messages._
 import io.mdcatapult.klein.mongo.Mongo
 import io.mdcatapult.klein.queue.Queue
 import io.mdcatapult.doclib.handlers.UnarchiveHandler
+import io.mdcatapult.doclib.util.MongoCodecs
+import org.bson.codecs.configuration.CodecRegistry
 import org.mongodb.scala.{Document, MongoCollection}
 
 import scala.concurrent.ExecutionContextExecutor
@@ -22,12 +24,13 @@ object ConsumerUnarchive extends App with LazyLogging {
   implicit val config: Config = ConfigFactory.load()
 
   /** Initialise Mongo **/
+  implicit val codecs: CodecRegistry = MongoCodecs.get
   implicit val mongo: Mongo = new Mongo()
   implicit val collection: MongoCollection[Document] = mongo.collection
 
   /** initialise queues **/
-  val downstream: Queue[PrefetchMsg] = new Queue[PrefetchMsg](config.getString("downstream.queue"))
-  val upstream: Queue[DoclibMsg] = new Queue[DoclibMsg](config.getString("upstream.queue"))
+  val downstream: Queue[PrefetchMsg] = new Queue[PrefetchMsg](config.getString("downstream.queue"), Option(config.getString("op-rabbit.topic-exchange-name")))
+  val upstream: Queue[DoclibMsg] = new Queue[DoclibMsg](config.getString("upstream.queue"), Option(config.getString("op-rabbit.topic-exchange-name")))
   val subscription: SubscriptionRef = upstream.subscribe(new UnarchiveHandler(downstream).handle, config.getInt("upstream.concurrent"))
 
 }
