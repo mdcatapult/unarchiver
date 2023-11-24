78b5433e9723941b00df8785c782b487e0cd016b
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2023-11-24 11:45:44 +0000

    Change method signatures to match alpakka from update queue lib

--- src/main/scala/io/mdcatapult/doclib/handlers/AnyHandlerResult.scala
+++ src/main/scala/io/mdcatapult/doclib/handlers/AnyHandlerResult.scala
@@ -1,3 +1,10 @@
 package io.mdcatapult.doclib.handlers
 
-case class AnyHandlerResult()
+import io.mdcatapult.doclib.consumer.HandlerResult
+import io.mdcatapult.doclib.models.DoclibDoc
+
+/**
+ * Convenience class for queues that you subscribe to for sending but don't care about the result
+ * @param doclibDoc
+ */
+case class AnyHandlerResult(doclibDoc: DoclibDoc) extends HandlerResult
